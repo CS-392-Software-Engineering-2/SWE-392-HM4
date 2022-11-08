94457a5837ef5d8fb89cacb9a75a7d614cfd170e
Felwa Bin Dakhil <89523599+felwadk@users.noreply.github.com>
2022-11-08 16:32:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function mooncakeSays(message) {
-  let mooncake = '(o.o)';
+  let mooncake = '(f.f)';
  console.log("Welcome to Programiz!");
 console.log("Welcome to Programiz!");
   if ( !message ) {
